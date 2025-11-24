78516b944d690717d04d90078e6a8f360eee787c
nicholas <nicho.tjornelund@gmail.com>
2025-11-24 18:07:38 +0100

    pull out the two libs

--- Kickoff/libdrawkit/shaders/texture/frag.glsl
+++
@@ -1,9 +0,0 @@
-R"(#version 100
-    precision mediump float;
-    varying vec2 vTexCoord;
-    uniform sampler2D texture0;
-    uniform vec4 color;
-    void main() {
-        gl_FragColor = texture2D(texture0, vTexCoord) * color;
-    }
-)"
--- Kickoff/libwayland-unstable/protocols/compositor.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMPOSITOR_H
-#define COMPOSITOR_H
-
-void compositor_init();
-void compositor_cleanup();
-
-struct wl_compositor *get_compositor();
-
-#endif
